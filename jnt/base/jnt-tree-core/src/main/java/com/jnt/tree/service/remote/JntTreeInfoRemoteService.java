88bf225fdf9c366a71d5a00b2d399208d66010d5
kang yang <kangyang1@lenovo.com>
2014-08-04 20:22:16 +0800

    update

--- jnt/base/jnt-tree-core/src/main/java/com/jnt/tree/service/remote/JntTreeInfoRemoteService.java
+++ jnt/base/jnt-tree-core/src/main/java/com/jnt/tree/service/remote/JntTreeInfoRemoteService.java
@@ -1,10 +1,13 @@
 package com.jnt.tree.service.remote;
 
+import org.oasisopen.sca.annotation.Remotable;
+
 /**
  * Created by arthur on 14-8-1.
  */
 
 
+@Remotable
 public interface JntTreeInfoRemoteService {
 
 }
---
+++ jnt/base/jnt-tree-service/src/main/java/com/jnt/tree/service/remote/impl/JntTreeInfoRemoteServiceImpl.java
@@ -0,0 +1,17 @@
+package com.jnt.tree.service.remote.impl;
+
+import com.jnt.tree.service.remote.JntTreeInfoRemoteService;
+import org.oasisopen.sca.annotation.Service;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kangyang1
+ * Date: 14-8-4
+ * Time: 下午4:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Service(JntTreeInfoRemoteService.class)
+@Component("JntTreeInfoRemoteService")
+public class JntTreeInfoRemoteServiceImpl implements JntTreeInfoRemoteService {
+}
