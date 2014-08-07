ac6f19d18a95dd220a62a55f0f28ed262d27bd4a
dizhenxiong <kangyang1@lenovo.com>
2014-08-07 12:45:22 +0800

    qq

--- jnt/base/jnt-tree-web/src/main/java/com/jnt/tree/web/controller/PlayerController.java
+++ jnt/base/jnt-tree-web/src/main/java/com/jnt/tree/web/controller/PlayerController.java
@@ -1,13 +1,10 @@
-package com.lenovo.vctl.apps.controller;
+package com.jnt.tree.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.jnt.tree.core.JntTree;
-import com.jnt.tree.service.remote.JntTreeRemoteService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PlayerController {
     private static final Log log = LogFactory.getLog(PlayerController.class);
 
-    @Autowired
-    public JntTreeRemoteService jntTreeRemoteService;
+//    @Autowired
+//    public JntTreeRemoteService jntTreeRemoteService;
 
     /**
      * 玩家登入
@@ -31,8 +28,8 @@ public class PlayerController {
     @RequestMapping(value = "/memory/tree")
          public String tree(HttpServletRequest request, HttpServletResponse response, ModelMap model, String name,
                              String password) throws Exception {
-        JntTree jntTree = jntTreeRemoteService.getJntTree(1l);
-        model.put("tree", jntTree);
+//        JntTree jntTree = jntTreeRemoteService.getJntTree(1l);
+//        model.put("tree", jntTree);
         return "simple1";
     }
 
@@ -42,9 +39,6 @@ public class PlayerController {
         return "simple1";
     }
 
-    public void setJntTreeRemoteService(JntTreeRemoteService jntTreeRemoteService) {
-        this.jntTreeRemoteService = jntTreeRemoteService;
-    }
 
 
 }
